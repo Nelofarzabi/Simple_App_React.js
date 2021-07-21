1dec354a86fdffd202f3d26942a72f8b45f91fa7
Nilofar <Nelofarzabi2000@gmail.com>
2021-07-21 06:02:45 -0700

    remove links style

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,8 +7,8 @@ class Navbar extends Component {
             <nav className="navbar">
                 <h1>The Dojo Blog </h1>
                 <div className= "Link">
-                <a href="/" style = {{color : "white" , backgroundColor:"#f1356d" ,borderRadius :"8px"}}>Home</a>
-                <a href="/">New Blog</a>
+                <a href="/" >Home</a>
+                <a href="/create">New Blog</a>
                
                 </div>
                
