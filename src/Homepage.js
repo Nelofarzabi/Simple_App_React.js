ecc0ff237e1dbf98c758d48093712d6fcc3619b6
Nilofar <Nelofarzabi2000@gmail.com>
2021-07-21 19:44:33 -0700

    change the content style

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
-const Home =()=>{
-    return(
-
+const Home = () => {
+    const [] = useState()
+    return (
+        <div className="home">
+            <h2>Home</h2>
+        </div>
 
     );
 
 }
-export default  Home ;
+export default Home;
