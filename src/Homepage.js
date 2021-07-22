5b043fd3d041d6a0a6f0bfcb40319d96eb95288f
Nilofar <Nelofarzabi2000@gmail.com>
2021-07-21 19:52:25 -0700

    define state in homepage

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
 const Home = () => {
-    const [] = useState()
+    const [Blogs , setBlogs] = useState([
+        {title : "My new website " , body : "Lorum ipsum .... " , autor : "mario" , id :1},
+        {title : "Welcome the party " , body : "Lorum ipsum .... " , autor : "yoshi" , id :2},
+        {title : "Web dev top tips " , body : "Lorum ipsum .... " , autor : "mario" , id :3}
+    ]);
     return (
         <div className="home">
             <h2>Home</h2>
+           
         </div>
 
     );
