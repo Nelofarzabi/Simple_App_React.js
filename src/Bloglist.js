e30da78b9a39df152f98835f51b9e565abc46ce2
Nilofar <Nelofarzabi2000@gmail.com>
2021-07-22 00:45:25 -0700

    edit bloglist file

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,14 +1,16 @@
 
-const Bloglist = (props) => {
-    let blogs = props.blogs;
-    let blogTitle = props.title;
+const Bloglist = ({blogs , title }) => {
+    // let blogs = props.blogs;
+    // let blogTitle = props.title;
+  
     return (
         <div className="blogList">
-            <h2>{blogTitle}</h2>
+            <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="preview-blog" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p> written by :{blog.autor}</p>
+                    {/* <button onClick={()=>}>Delete Me</button> */}
 
 
                 </div>
