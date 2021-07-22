0bc2bf9ca5444b22e7a24d924434bb9b2b1f366d
Nilofar <Nelofarzabi2000@gmail.com>
2021-07-21 19:30:45 -0700

    edit home page

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,28 +1,10 @@
-import React , { Component } from "react";
+import { useState } from "react";
 
-class Home extends Component{
-   
-    render() {
-        const  show = (name) => {
-            console.log("hello janm");
-        };
-        const hide = (name) =>{
-            console.log ("hello" + name  )
-        }
-     
-        return (
-            <div className ="homePage">
-            <h2>Home Page </h2>
-         <button onClick = {show()}
-         >Show Me</button>
-         <button onClick={()=>{
-             hide('nelofar')
-         }} >Hide Me</button>
+const Home =()=>{
+    return(
 
-            
-            </div>
-        );
-    }
+
+    );
 
 }
-export default Home ;
+export default  Home ;
