511582576a7d9ad3e13c00b810dcb0cbc68ffcad
Nilofar <Nelofarzabi2000@gmail.com>
2021-07-21 17:41:13 -0700

    add button

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,11 +1,23 @@
 import React , { Component } from "react";
 
 class Home extends Component{
+   
     render() {
+        const  show = (name) => {
+            console.log("hello janm");
+        };
+        const hide = (name) =>{
+            console.log ("hello" + name  )
+        }
+     
         return (
             <div className ="homePage">
             <h2>Home Page </h2>
-         
+         <button onClick = {show()}
+         >Show Me</button>
+         <button onClick={()=>{
+             hide('nelofar')
+         }} >Hide Me</button>
 
             
             </div>
